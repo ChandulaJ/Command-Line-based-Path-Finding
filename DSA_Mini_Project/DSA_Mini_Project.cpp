20e1fdf04b7ef9a627b448dd39688b735184edc7
Inupa Vihan <inupavihan9@gmail.com>
2023-01-20 10:32:05 +0530

    Updated Display Output
    Changes
    -Replaced all the arrays with vectors or dynamic 2D arrays
    -Removed the map.txt file which is previously created
    -Added a new map.txt file inside the project folder for user inputs
    -Modified the code to take the user input for the map from a text file
    -Optimized and updated the functions related to the output display
    
    Issues
    -Need further optimizations
    -Need further verification of the code changes
    -Need to check errors which may occur due to code changes

--- DSA_Mini_Project/DSA_Mini_Project.cpp
+++ DSA_Mini_Project/DSA_Mini_Project.cpp
@@ -2,14 +2,17 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include "Color.h" //header file to change the color in the terminal
 #include "Functions.h"
 
 /*
 * Instructions :
 *
+* Insert the height and width repectively in the first line in map.txt which is inside the project folder
+* Insert the map to the same map.txt file
 * Run the project
-* Copy and paste all the content in the map.txt file and hit enter to input the map
 * Nodes corresponding to the shortest path will be highlighted in cyan (light blue) color
 * Maximum map size is 52 x 52
 *
@@ -20,17 +23,68 @@ using namespace std;
 int main()
 {
     //taking the user input for the map
-    int map[43][30];
+    //int mapArr[43][30];
+    string path = "map.txt";
 
+    ifstream fin(path);
+
+    //getting the height and width of the map
+    int y, x;
+    fin >> y >> x;
+
+    vector<vector<int>> map(y, vector<int>(x));
+
+    //reading the map from a text file
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            int d;
+            fin >> d;
+            map[i][j] = d;
+            //mapArr[i][j] = d;
+        }
+    }
+
+    //y = 43;
+    //x = 30;
+
+    /*
+    int** mapArr = new int* [y];
+    for (int i = 0; i < y; i++) {
+        mapArr[i] = new int[x];
+    }
+    
+    
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            mapArr[i][j] == map[i][j];
+        }
+    }
+    */
+
+    /*
     //enter the map in the map.txt file
     for (int i = 0; i < 43; i++) {
         for (int j = 0; j < 30; j++) {
             int d;
             cin >> d;
-            map[i][j] = d;
+            mapArr[i][j] = d;
         }
         cout << endl;
     }
+    */
+
+    /*
+    //enter the map in the map.txt file
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            int d;
+            cin >> d;
+            map[i][j] = d;
+            mapArr[i][j] = d;
+        }
+        cout << endl;
+    }
+    */
 
     cout << "===============================================================" << endl;
     cout << "Finding the shortest path in a map" << endl;
@@ -39,12 +93,12 @@ int main()
     cout << endl;
 
     //displaying map for the user and getting inputs for starting point and destination point
-    display_Map(map);
+    display_Map(map, findNodes(path), path);
 
     //displaying the shortest path
     display_shorestpath();
 
     //displaying the shortest path in the map
-    print_Map(map);
+    print_Map(map, findNodes(path), path);
 }
 
